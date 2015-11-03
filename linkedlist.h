261de2550d1e65d4f1fae50c7570cb6c53cd4dc5
Giselle Serate <giselle.serate@gmail.com>
2015-11-03 14:39:09 -0800

    Added ifndef and define to linkedlist.h

--- linkedlist.h
+++ linkedlist.h
@@ -1,10 +1,15 @@
+#ifndef _linkedlist_h
+#define _linkedlist_h
+
 struct Node {
 	struct Node *next;
 	char *arc_label;
-	//Are we referring the rooms as structs themselves or are we referring to them as character strings. 
+	//Are we referring the rooms as structs themselves or are we referring to them as character strings? 
 	char rm;
 };
 
 struct List {
 	struct Node *first;
 };
+
+#endif
