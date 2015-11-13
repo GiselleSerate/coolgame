4c223a541a92816964de2721a483ccfe7c55130f
Giselle Serate <giselle.serate@gmail.com>
2015-11-12 16:04:43 -0800

    Made int pointers work and print in an array

--- main.c
+++ main.c
@@ -7,13 +7,15 @@
 int main(int argc, char *argv[]) {
 	//List = {};
 	int onex = 1;
-	// int twox = 2;
+	int twox = 2;
 	int *one = &onex;
-	// int *two = twox;
+	int *two = &twox;
 	//following line is iffy
-	// int* listArray[] = {one,two};
+	int* listArray[] = {one,two};
 	// int *myPointer = listArray;
-	printf("listArray is %d", *one);
+	printf("listArray is %d", **listArray);
+	printf("listArray is %d", *listArray[1]);
+
 	// printf("listArray is %d", *listArray[0]);
 	//struct Graph *myMap = Graph_create(myPointer);
 	return 0;
